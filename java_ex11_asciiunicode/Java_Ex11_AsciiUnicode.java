446a2f171da95675ef082d9336268df9b3bbdacf
Jhonatan <jhonatanmetrio@gmail.com>
2019-01-22 13:59:00 -0500

    Read line of text

---
+++ java_ex11_asciiunicode/Java_Ex11_AsciiUnicode.java
@@ -0,0 +1,25 @@
+/**
+ * Cay S. Horstmann -- Chapter 1
+ * Exercise #11
+ * Write a program that reads a line of text and prints all characters 
+ * that are not ASCII, together with their Unicode values.
+ */
+package java_ex11_asciiunicode;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jhonatan
+ */
+public class Java_Ex11_AsciiUnicode {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+    }
+    
+}
